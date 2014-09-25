fa2dacca50e6751505b776dca965dca068b14bfd
Dmitri Urbanowicz <hun10@yandex.ru>
2014-09-26 00:31:36 +0400

    [ref]

--- play.js
+++ play.js
@@ -1,26 +1,41 @@
-var audio = new Audio(); // create the HTML5 audio element
-var freq = Math.PI * 2 * 440 / 44100;
+var Synth = function() {
+    var audio = new Audio();
+    var wave = new RIFFWAVE();
+
+    wave.header.sampleRate = 44100;
+    wave.header.numChannels = 1;
+    wave.header.bitsPerSample = 16;
+
+    function w(semitone) {
+        return Math.PI * 2 * 440 * Math.pow(2, semitone / 12) / 44100;
+    }
+
+    function genTone(semitone) {
+        var data = [];
+        var freq = w(semitone);
+        for (var i = 0; i < wave.header.sampleRate / 4; i++) {
+            data[i] = Math.round(32000 * Math.sin(freq * i));
+        }
+        return data;
+    }
+
+    function genToneSeq(seq) {
+        var data = [];
+        for (var i = 0; i < seq.length; i++) {
+            data.push.apply(data, genTone(seq[i]));
+        }
+        return data;
+    }
+
+    return {
+        play: function(seq) {
+            wave.Make(genToneSeq(seq));
+            audio.src = wave.dataURI;
+            audio.play();
+        }
+    };
+}();
 
 function Play() {
-
-var wave = new RIFFWAVE(); // create an empty wave file
-var data = []; // yes, it's an array
-
-wave.header.sampleRate = 44100; // set sample rate to 44KHz
-wave.header.numChannels = 1;
-wave.header.bitsPerSample = 16;
-
-var i = 0;
-var sq = Math.pow(2, 1/12);
-
-while (i<130000) {
-  data[i++] = Math.round(12007*Math.sin(freq * i));
-  if (i%10000 == 0) {
-    freq *= sq;
-  }
-}
-
-wave.Make(data); // make the wave file
-audio.src = wave.dataURI; // set audio source
-audio.play(); // we should hear two tones one on each speaker
+    Synth.play([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
 }
