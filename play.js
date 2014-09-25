18d0b838fd5c70fabb126c96b7d990cef4e32cfa
Dmitri Urbanowicz <hun10@yandex.ru>
2014-09-25 10:30:47 +0400

    [fix] A of first octave

--- play.js
+++ play.js
@@ -5,14 +5,13 @@ var wave = new RIFFWAVE(); // create an empty wave file
 var data = []; // yes, it's an array
 
 wave.header.sampleRate = 44100; // set sample rate to 44KHz
-wave.header.numChannels = 2; // two channels (stereo)
+wave.header.numChannels = 1;
 wave.header.bitsPerSample = 16;
 
 var i = 0;
 var freq = Math.PI * 2 * 440 / 44100;
 while (i<100000) { 
-  data[i++] = Math.round(12007*Math.sin(freq * i)); // left speaker
-  data[i++] = Math.round(12007*Math.sin(freq * i)); // right speaker
+  data[i++] = Math.round(12007*Math.sin(freq * i));
 }
 
 wave.Make(data); // make the wave file
