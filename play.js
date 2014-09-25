3d809e35cbe26e099fd51f99640549d529f804eb
Dmitri Urbanowicz <hun10@yandex.ru>
2014-09-26 00:34:30 +0400

    [+] Major scale

--- play.js
+++ play.js
@@ -13,7 +13,7 @@ var Synth = function() {
     function genTone(semitone) {
         var data = [];
         var freq = w(semitone);
-        for (var i = 0; i < wave.header.sampleRate / 4; i++) {
+        for (var i = 0; i < wave.header.sampleRate / 3; i++) {
             data[i] = Math.round(32000 * Math.sin(freq * i));
         }
         return data;
@@ -37,5 +37,5 @@ var Synth = function() {
 }();
 
 function Play() {
-    Synth.play([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+    Synth.play([0, 2, 4, 5, 7, 9, 11, 12]);
 }
