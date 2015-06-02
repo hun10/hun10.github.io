b24992d8315c7819458c4326199c5c96240b2551
Dmitri Urbanowicz <hun10@yandex.ru>
2015-06-02 16:37:34 +0400

    [+] Playable

--- schulte/schulte.js
+++ schulte/schulte.js
@@ -51,11 +51,62 @@ function showTable(cells, colorsArray) {
   return table;
 }
 
+
+function showHints(colorsArray) {
+  var container = $("<div>", { style: "margin: 1.5em;"});
+  for (var i = 0; i < colorsArray.length; i++) {
+    var style = "background-color: " + colorsArray[i];
+    style += "; padding: 1em; display: inline; border: 1px solid; margin: 0.5em"
+    var el = $("<div>", { text: i, style: style });
+    container.append(el);
+  }
+  return container;
+}
+
 function main() {
-  var cells = makeRandomTable(5, 2);
+  var cells = makeRandomTable(3, 2);
   var colors = ["lightblue", "white"];
-  var table = showTable(cells, colors)
-  $("body").append($("<center>").append(table));
+  var container = $("<center>");
+  var table = showTable(cells, colors);
+  container.append(table);
+  var input = $("<input>");
+  container.append(input);
+  container.append($("<p>", { text: "Use these numbers to enumerate the colors in order:" }));
+  var hints = showHints(colors);
+  container.append(hints);
+  $("body").append(container);
+  input.focus();
+  function wrongAnswer() {
+    alert("Wrong!");
+  }
+  function rightAnswer() {
+    alert("Ok.");
+  }
+  function checkAnswer() {
+    var answer = input.val();
+    if (answer.length == cells.length * cells.length) {
+      for (var k = 0; k < answer.length; k++) {
+        var c = Number(answer[k]);
+        var found = false;
+        for (var i = 0; i < cells.length; i++) {
+          for (var j = 0; j < cells[i].length; j++) {
+            if (cells[i][j].num == k + 1) {
+              found = cells[i][j].color == c;
+              break;
+            }
+          }
+        }
+        if (!found) {
+          wrongAnswer();
+          return;
+        }
+      }
+      rightAnswer();
+      return;
+    }
+    wrongAnswer();
+  }
+  input.keypress(function(e) { if (e.keyCode == 13) checkAnswer(); });
 }
 
 $(main);
