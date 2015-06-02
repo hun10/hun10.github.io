c9444d5dcd796339e2dae190c8b9d3695b2fbe03
Dmitri Urbanowicz <hun10@yandex.ru>
2015-06-02 15:10:08 +0400

    [+] Building actual Schulte table

---
+++ schulte/schulte.js
@@ -0,0 +1,60 @@
+function assert(expression) {
+  if (!expression) {
+    throw new Error("Assertion violated.");
+  }
+}
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function makeRandomTable(size, colors) {
+  assert(size > 0 && size % 2 != 0);
+  assert(colors > 0);
+  plain = [];
+  for (var i = 0; i < size * size; i++) {
+    plain.push(i + 1);
+  }
+  shuffleArray(plain);
+  table = [];
+  for (var i = 0, c = 0; i < size; i++) {
+    table.push([]);
+    for (var j = 0; j < size; j++) {
+      table[i][j] = {
+        num : plain[c],
+        color : c % colors
+      };
+      c++;
+    }
+  }
+  return table;
+}
+
+function showTable(cells, colorsArray) {
+  var table = $("<table>", { style: "border: 1px solid" });
+  for (var i = 0; i < cells.length; i++) {
+    var row = $("<tr>");
+    for (var j = 0; j < cells[i].length; j++) {
+      var style = "background-color: " + colorsArray[cells[i][j].color];
+      style += "; width: 2em; height: 2em; vertical-align: middle; text-align: center"
+      var cell = $("<td>", { text: cells[i][j].num, style: style });
+      row.append(cell);
+    }
+    table.append(row);
+  }
+  return table;
+}
+
+function main() {
+  var cells = makeRandomTable(5, 2);
+  var colors = ["lightblue", "white"];
+  $("body").append(showTable(cells, colors));
+}
+
+$(main);
