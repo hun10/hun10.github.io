b1de07881ce0290da800191cec2f72e6b6f4dbd8
Dmitri Urbanowicz <hun10@yandex.ru>
2020-12-30 15:16:34 +0400

    what

--- vr/models/main.js
+++ vr/models/main.js
@@ -1,6 +1,3 @@
-import { WebXRPolyfill } from './webxr-polyfill.js';
-const polyfill = new WebXRPolyfill();
-
 import * as THREE from '../build/three.module.js';
 import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
 import { RGBELoader } from './jsm/loaders/RGBELoader.js';
