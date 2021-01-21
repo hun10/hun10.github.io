1f4d8f801216861e587841c9018866764b73bfb1
Dmitri Urbanowicz <hun10@yandex.ru>
2021-01-21 17:18:55 +0400

    upd

--- vr/ar/main.js
+++ vr/ar/main.js
@@ -8,210 +8,233 @@ var sceneGroup;
 
 var globe;
 
+const relPos = [];
+let calComplete = false;
+const tmpVec = new THREE.Vector3();
+
 initialize();
 animate();
 
 function initialize()
 {
-	scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-	let ambientLight = new THREE.AmbientLight( 0xcccccc, 0.5 );
-	scene.add( ambientLight );
+    let ambientLight = new THREE.AmbientLight( 0xcccccc, 0.5 );
+    scene.add( ambientLight );
 
-	camera = new THREE.Camera();
-	scene.add(camera);
+    camera = new THREE.Camera();
+    scene.add(camera);
 
-	renderer = new THREE.WebGLRenderer({
-		antialias : true,
-		alpha: true
-	});
-	renderer.setClearColor(new THREE.Color('lightgrey'), 0)
-	renderer.setSize( 800, 600 );
-	renderer.domElement.style.position = 'absolute'
-	renderer.domElement.style.top = '0px'
-	renderer.domElement.style.left = '0px'
-	document.body.appendChild( renderer.domElement );
+    renderer = new THREE.WebGLRenderer({
+        antialias : true,
+    alpha: true
+    });
+    renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+    renderer.setSize( 800, 600 );
+    renderer.domElement.style.position = 'absolute'
+    renderer.domElement.style.top = '0px'
+    renderer.domElement.style.left = '0px'
+    document.body.appendChild( renderer.domElement );
 
-	clock = new THREE.Clock();
-	deltaTime = 0;
-	totalTime = 0;
-	
-	////////////////////////////////////////////////////////////
-	// setup arToolkitSource
-	////////////////////////////////////////////////////////////
+    clock = new THREE.Clock();
+    deltaTime = 0;
+    totalTime = 0;
 
-	arToolkitSource = new THREEx.ArToolkitSource({
-		sourceType : 'webcam',
-	});
+    ////////////////////////////////////////////////////////////
+    // setup arToolkitSource
+    ////////////////////////////////////////////////////////////
 
-	function onResize()
-	{
-		arToolkitSource.onResize()	
-		arToolkitSource.copySizeTo(renderer.domElement)	
-		if ( arToolkitContext.arController !== null )
-		{
-			arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)	
-		}	
-	}
+    arToolkitSource = new THREEx.ArToolkitSource({
+        sourceType : 'webcam',
+    });
 
-	arToolkitSource.init(function onReady(){
-		onResize()
-	});
-	
-	// handle resize event
-	window.addEventListener('resize', function(){
-		onResize()
-	});
-	
-	////////////////////////////////////////////////////////////
-	// setup arToolkitContext
-	////////////////////////////////////////////////////////////	
+    function onResize()
+    {
+        arToolkitSource.onResize()
+        arToolkitSource.copySizeTo(renderer.domElement)
+        if ( arToolkitContext.arController !== null )
+        {
+            arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)
+        }
+    }
 
-	// create atToolkitContext
-	arToolkitContext = new THREEx.ArToolkitContext({
-		cameraParametersUrl: 'data/camera_para.dat',
-		detectionMode: 'mono',
-		maxDetectionRate: 30,
-	});
-	
-	// copy projection matrix to camera when initialization complete
-	arToolkitContext.init( function onCompleted(){
-		camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
-	});
+    arToolkitSource.init(function onReady(){
+        onResize()
+    });
 
-	////////////////////////////////////////////////////////////
-	// setup markerRoots
-	////////////////////////////////////////////////////////////
+    // handle resize event
+    window.addEventListener('resize', function(){
+        onResize()
+    });
 
-	markerNames = ["kanji", "letterA", "letterB", "letterC"];
-	
-	markerArray = [];
-	
-	for (let i = 0; i < markerNames.length; i++)
-	{
-		let marker = new THREE.Group();
-		scene.add(marker);
-		markerArray.push(marker);
-		
-		let markerControls = new THREEx.ArMarkerControls(arToolkitContext, marker, {
-			type: 'pattern', patternUrl: "data/" + markerNames[i] + ".patt",
-		});
-		
-		let markerGroup = new THREE.Group();
-		marker.add(markerGroup);
-	}
-	
-	////////////////////////////////////////////////////////////
-	// setup scene
-	////////////////////////////////////////////////////////////
-	
-	sceneGroup = new THREE.Group();
-	
-	let loader = new THREE.TextureLoader();
-	
-	let geometry1 = new THREE.SphereGeometry(1, 32,32);	
-	let texture = loader.load( 'images/earth-sphere.jpg' );
-	let material1 = new THREE.MeshLambertMaterial( { map: texture, opacity: 0.75 } );
-	globe = new THREE.Mesh( geometry1, material1 );
-	globe.position.y = 1;
-	sceneGroup.add(globe); 
-	
-	scene.add( sceneGroup );
-	currentMarkerName = markerNames[0];
-	
-	let pointLight = new THREE.PointLight( 0xffffff, 1, 50 );
-	camera.add( pointLight );
+    ////////////////////////////////////////////////////////////
+    // setup arToolkitContext
+    ////////////////////////////////////////////////////////////
+
+    // create atToolkitContext
+    arToolkitContext = new THREEx.ArToolkitContext({
+    cameraParametersUrl: 'data/camera_para.dat',
+    detectionMode: 'mono',
+    maxDetectionRate: 30,
+    });
+
+    // copy projection matrix to camera when initialization complete
+    arToolkitContext.init( function onCompleted(){
+        camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
+    });
+
+    ////////////////////////////////////////////////////////////
+    // setup markerRoots
+    ////////////////////////////////////////////////////////////
+
+    markerNames = ["kanji", "letterA", "letterB", "letterC"];
+
+    markerArray = [];
+
+    for (let i = 0; i < markerNames.length; i++)
+    {
+        let marker = new THREE.Group();
+        scene.add(marker);
+        markerArray.push(marker);
+
+        let markerControls = new THREEx.ArMarkerControls(arToolkitContext, marker, {
+        type: 'pattern', patternUrl: "data/" + markerNames[i] + ".patt",
+        });
+
+        let markerGroup = new THREE.Group();
+        marker.add(markerGroup);
+    }
+
+    ////////////////////////////////////////////////////////////
+    // setup scene
+    ////////////////////////////////////////////////////////////
+
+    sceneGroup = new THREE.Group();
+
+    let loader = new THREE.TextureLoader();
+
+    let geometry1 = new THREE.SphereGeometry(1, 32,32);
+    let texture = loader.load( 'images/earth-sphere.jpg' );
+    let material1 = new THREE.MeshLambertMaterial( { map: texture, opacity: 0.75 } );
+    globe = new THREE.Mesh( geometry1, material1 );
+    globe.position.y = 4;
+    sceneGroup.add(globe);
+
+    scene.add( sceneGroup );
+    currentMarkerName = markerNames[0];
+
+    let pointLight = new THREE.PointLight( 0xffffff, 1, 50 );
+    camera.add( pointLight );
 }
-
 
 function update()
 {
-	
-	globe.rotation.y += 0.01;
 
-let c = new THREE.Vector3();
-let count = 0;
-for (let i = 0; i < markerArray.length; i++) {
-  if (markerArray[i].visible) {
-    c.add( markerArray[i].position );
-    count++;
-  }
-}
+    //	globe.rotation.y += 0.01;
 
-c.divideScalar(count);
+    if (!calComplete) {
+        let baseMarker = markerArray[0];
 
-sceneGroup.position.copy(c);
+        let c = new THREE.Vector3();
+        let count = 0;
 
-/*	let anyMarkerVisible = false;
-	for (let i = 0; i < markerArray.length; i++)
-	{
-		if ( markerArray[i].visible )
-		{
-			anyMarkerVisible = true;
-			markerArray[i].children[0].add( sceneGroup );
-			if ( currentMarkerName != markerNames[i] )
-			{
-				currentMarkerName = markerNames[i];
-				// console.log("Switching to " + currentMarkerName);
-			}
-			
-			let p = markerArray[i].children[0].getWorldPosition();
-			let q = markerArray[i].children[0].getWorldQuaternion();
-			let s = markerArray[i].children[0].getWorldScale();
-			let lerpAmount = 0.5;
-			
-			scene.add(sceneGroup);
-			sceneGroup.position.lerp(p, lerpAmount);
-			sceneGroup.quaternion.slerp(q, lerpAmount);
-			sceneGroup.scale.lerp(s, lerpAmount);
+        // update relative positions of markers
+        for (let i = 1; i < markerArray.length; i++)
+        {
+            let currentMarker = markerArray[i];
+            let currentGroup  = currentMarker.children[0];
+            if ( baseMarker.visible && currentMarker.visible )
+            {
+                // console.log("updating marker " + i " -> base offset");
 
-			break;
-		}
-	}
+                let relativePosition = currentMarker.worldToLocal( baseMarker.position.clone() );
+                c.add(relativePosition);
 
-	if ( !anyMarkerVisible )
-	{
-		// console.log("No marker currently visible.");
-	}
-	
-	let baseMarker = markerArray[0];
-	
-	// update relative positions of markers
-	for (let i = 1; i < markerArray.length; i++)
-	{
-		let currentMarker = markerArray[i];
-		let currentGroup  = currentMarker.children[0];
-		if ( baseMarker.visible && currentMarker.visible )
-		{
-			// console.log("updating marker " + i " -> base offset");
-			
-			let relativePosition = currentMarker.worldToLocal( baseMarker.position.clone() );
-			currentGroup.position.copy( relativePosition );
-			
-			let relativeRotation = currentMarker.quaternion.clone().inverse().multiply( baseMarker.quaternion.clone() );
-			currentGroup.quaternion.copy( relativeRotation );
-		}
-	}
-*/	
-	// update artoolkit on every frame
-	if ( arToolkitSource.ready !== false )
-		arToolkitContext.update( arToolkitSource.domElement );
+                count++;
+            }
+        }
+
+        if (count + 1 === markerArray.length) {
+
+            c.divideScalar(count + 1);
+
+            globe.translateOnAxis(c, -1);
+
+            calComplete = true;
+        }
+
+    } else {
+        let anyMarkerVisible = false;
+        for (let i = 0; i < markerArray.length; i++)
+        {
+            if ( markerArray[i].visible )
+            {
+                anyMarkerVisible = true;
+                markerArray[i].children[0].add( sceneGroup );
+                if ( currentMarkerName != markerNames[i] )
+                {
+                    currentMarkerName = markerNames[i];
+                    // console.log("Switching to " + currentMarkerName);
+                }
+
+                let p = markerArray[i].children[0].getWorldPosition();
+                let q = markerArray[i].children[0].getWorldQuaternion();
+                let s = markerArray[i].children[0].getWorldScale();
+                let lerpAmount = 0.5;
+
+                scene.add(sceneGroup);
+                sceneGroup.position.lerp(p, lerpAmount);
+                sceneGroup.quaternion.slerp(q, lerpAmount);
+                sceneGroup.scale.lerp(s, lerpAmount);
+
+                break;
+            }
+        }
+
+        if ( !anyMarkerVisible )
+        {
+            // console.log("No marker currently visible.");
+        }
+
+        let baseMarker = markerArray[0];
+
+        // update relative positions of markers
+        for (let i = 1; i < markerArray.length; i++)
+        {
+            let currentMarker = markerArray[i];
+            let currentGroup  = currentMarker.children[0];
+            if ( baseMarker.visible && currentMarker.visible )
+            {
+                // console.log("updating marker " + i " -> base offset");
+
+                let relativePosition = currentMarker.worldToLocal( baseMarker.position.clone() );
+                currentGroup.position.copy( relativePosition );
+
+                let relativeRotation = currentMarker.quaternion.clone().inverse().multiply( baseMarker.quaternion.clone() );
+                currentGroup.quaternion.copy( relativeRotation );
+            }
+        }
+    }
+
+    // update artoolkit on every frame
+    if ( arToolkitSource.ready !== false )
+        arToolkitContext.update( arToolkitSource.domElement );
 
 }
 
 
 function render()
 {
-	renderer.render( scene, camera );
+    renderer.render( scene, camera );
 }
 
 
 function animate()
 {
-	requestAnimationFrame(animate);
-	deltaTime = clock.getDelta();
-	totalTime += deltaTime;
-	update();
-	render();
+    requestAnimationFrame(animate);
+    deltaTime = clock.getDelta();
+    totalTime += deltaTime;
+    update();
+    render();
 }
 
