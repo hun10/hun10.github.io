d325dd32a8a122778599f98594d8919096fe35c4
Dmitry Urbanovich <hun10@yandex.ru>
2022-07-22 15:48:39 +0300

    Create main-worklet.js

---
+++ main-worklet.js
@@ -0,0 +1,12 @@
+const button = document.createElement('button')
+button.innerText = 'Play!'
+
+button.onclick = function() {
+    const audioContext = new AudioContext()
+    audioContext.audioWorklet.addModule('audio-processor.js').then(() => {
+        const randomNoiseNode = new AudioWorkletNode(audioContext, 'audio-processor')
+        randomNoiseNode.connect(audioContext.destination)
+    })
+}
+
+document.body.appendChild(button)
