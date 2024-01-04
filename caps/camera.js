a818bbc92cd273d794b95b519d4377d7e668f667
Dmitri Urbanowicz <urbanowicz@Dmitris-MacBook-Pro.local>
2024-01-04 15:53:25 +0300

    camera

--- caps/camera.js
+++ caps/camera.js
@@ -1,3 +1,9 @@
+import buffersGl from './buffers-gl.js'
+import { numericControl, button } from './controls.js'
+
+const canvas = document.createElement('canvas')
+document.body.appendChild(canvas)
+
 const select = document.createElement('select')
 document.body.appendChild(select)
 
@@ -17,27 +23,140 @@ async function main() {
 
     choose()
     select.addEventListener('change', choose)
+
+    const gl = canvas.getContext('webgl2', {
+        depth: false,
+        antialias: false
+    })
+    gl.disable(gl.BLEND)
+    gl.disable(gl.CULL_FACE)
+    gl.disable(gl.DEPTH_TEST)
+    gl.disable(gl.DITHER)
+    gl.disable(gl.POLYGON_OFFSET_FILL)
+    gl.disable(gl.SAMPLE_ALPHA_TO_COVERAGE)
+    gl.disable(gl.SAMPLE_COVERAGE)
+    gl.disable(gl.STENCIL_TEST)
+    gl.disable(gl.RASTERIZER_DISCARD)
+    gl.disable(gl.SCISSOR_TEST)
+
+    gl.getExtension('EXT_color_buffer_float')
+    gl.getExtension('OES_texture_float_linear')
+
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
+
+    const {
+        screen,
+        pixelBuffer,
+        doublePixelBuffer,
+        shader,
+    } = buffersGl(gl)
+
+    const sourceBuffer = pixelBuffer({
+        filter: gl.NEAREST,
+        format: gl.RGBA32F,
+        access: gl.RGBA,
+        store: gl.FLOAT
+    })
+    const finalBuffer = doublePixelBuffer({
+        filter: gl.NEAREST,
+        format: gl.RGBA32F,
+        access: gl.RGBA,
+        store: gl.FLOAT
+    })
+
+    const accumulate = shader(`
+        out vec4 rgbc;
+
+        precision highp sampler2D;
+        uniform sampler2D u_source;
+        uniform sampler2D u_back;
+
+        void main() {
+            vec4 sRgbSource = texelFetch(u_source, ivec2(gl_FragCoord.xy), 0);
+            vec4 linearBack = texelFetch(u_back, ivec2(gl_FragCoord.xy), 0);
+
+            vec3 linearRgb = toLinear(sRgbSource.rgb);
+            linearBack.rgb *= linearBack.a / (linearBack.a + 1.0);
+            linearBack.rgb += linearRgb / (linearBack.a + 1.0);
+            linearBack.a += 1.0;
+
+            rgbc = linearBack;
+        }
+    `)
+
+    const toDitheredSrgb = shader(`
+        out vec4 sRgb;
+
+        precision highp sampler2D;
+        uniform sampler2D u_source;
+        uniform float u_exposure;
+
+        void main() {
+            vec4 fetched = texelFetch(u_source, ivec2(gl_FragCoord.xy), 0);
+            vec3 exposed = u_exposure * fetched.rgb;
+
+            sRgb = ditherFinal(fromLinear(
+                exposed
+            ));
+        }
+    `)
+
+    const video = document.getElementById('preview')
+
+    const exposure = numericControl('Exposure', -10, 10, 0.001, 0)
+    let accumulation = false
+    button('Toggle Accumulation', () => {
+        accumulation = !accumulation
+    })
+
+    while (true) {
+        await frame()
+
+        sourceBuffer.resize()
+        sourceBuffer.update(video)
+
+        finalBuffer.resize()
+        if (!accumulation) {
+            finalBuffer.update()
+        }
+        accumulate.draw({
+            u_source: sourceBuffer,
+            u_back: finalBuffer,
+        }, finalBuffer)
+
+        toDitheredSrgb.draw({
+            u_source: finalBuffer,
+            u_exposure: Math.pow(2, exposure.value)
+        }, screen)
+    }
 }
 
 async function choose() {
     const stream = await navigator.mediaDevices.getUserMedia({
         video: {
-            deviceId: select.value
+            deviceId: select.value,
+            width: 1e9,
+            height: 1e9
         }
     })
 
     const caps = stream.getVideoTracks().map(track => ({[track.label]: track.getCapabilities()}))
+
+    console.log(caps)
     pre.innerText = JSON.stringify(caps, null, 2)
 
-    const sets = stream.getVideoTracks().map(track => ({[track.label]: track.getSettings()}))
-    pre.innerText += JSON.stringify(sets, null, 2)
+    const video = document.getElementById('preview')
+    const trackSettings = stream.getVideoTracks()[0].getSettings()
+    pre.innerText += JSON.stringify(trackSettings, null, 2)
 
-    const vid = document.createElement('video')
-    vid.srcObject = stream
-    vid.muted = true
-    document.body.appendChild(vid)
+    canvas.width = trackSettings.width
+    canvas.height = trackSettings.height
 
-    await vid.play()
+    video.srcObject = stream
 }
 
-window.onload = () => main().catch(alert)
+function frame() {
+    return new Promise(resolve => requestAnimationFrame(resolve))
+}
+
+main().catch(e => document.body.innerText = e)
