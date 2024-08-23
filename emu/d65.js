c54d1eb831dcacd402b6850d7f879bed5e19bbcf
urbanowicz <urbanowicz@local>
2024-08-23 20:11:04 +0400

    m

---
+++ emu/d65.js
@@ -0,0 +1,390 @@
+export default [
+    {
+       "power": 0.0341,
+       "wavelength": 300
+    },
+    {
+       "power": 1.6643,
+       "wavelength": 305
+    },
+    {
+       "power": 3.2945,
+       "wavelength": 310
+    },
+    {
+       "power": 11.7652,
+       "wavelength": 315
+    },
+    {
+       "power": 20.236,
+       "wavelength": 320
+    },
+    {
+       "power": 28.6447,
+       "wavelength": 325
+    },
+    {
+       "power": 37.0535,
+       "wavelength": 330
+    },
+    {
+       "power": 38.5011,
+       "wavelength": 335
+    },
+    {
+       "power": 39.9488,
+       "wavelength": 340
+    },
+    {
+       "power": 42.4302,
+       "wavelength": 345
+    },
+    {
+       "power": 44.9117,
+       "wavelength": 350
+    },
+    {
+       "power": 45.775,
+       "wavelength": 355
+    },
+    {
+       "power": 46.6383,
+       "wavelength": 360
+    },
+    {
+       "power": 49.3637,
+       "wavelength": 365
+    },
+    {
+       "power": 52.0891,
+       "wavelength": 370
+    },
+    {
+       "power": 51.0323,
+       "wavelength": 375
+    },
+    {
+       "power": 49.9755,
+       "wavelength": 380
+    },
+    {
+       "power": 52.3118,
+       "wavelength": 385
+    },
+    {
+       "power": 54.6482,
+       "wavelength": 390
+    },
+    {
+       "power": 68.7015,
+       "wavelength": 395
+    },
+    {
+       "power": 82.7549,
+       "wavelength": 400
+    },
+    {
+       "power": 87.1204,
+       "wavelength": 405
+    },
+    {
+       "power": 91.486,
+       "wavelength": 410
+    },
+    {
+       "power": 92.4589,
+       "wavelength": 415
+    },
+    {
+       "power": 93.4318,
+       "wavelength": 420
+    },
+    {
+       "power": 90.057,
+       "wavelength": 425
+    },
+    {
+       "power": 86.6823,
+       "wavelength": 430
+    },
+    {
+       "power": 95.7736,
+       "wavelength": 435
+    },
+    {
+       "power": 104.865,
+       "wavelength": 440
+    },
+    {
+       "power": 110.936,
+       "wavelength": 445
+    },
+    {
+       "power": 117.008,
+       "wavelength": 450
+    },
+    {
+       "power": 117.41,
+       "wavelength": 455
+    },
+    {
+       "power": 117.812,
+       "wavelength": 460
+    },
+    {
+       "power": 116.336,
+       "wavelength": 465
+    },
+    {
+       "power": 114.861,
+       "wavelength": 470
+    },
+    {
+       "power": 115.392,
+       "wavelength": 475
+    },
+    {
+       "power": 115.923,
+       "wavelength": 480
+    },
+    {
+       "power": 112.367,
+       "wavelength": 485
+    },
+    {
+       "power": 108.811,
+       "wavelength": 490
+    },
+    {
+       "power": 109.082,
+       "wavelength": 495
+    },
+    {
+       "power": 109.354,
+       "wavelength": 500
+    },
+    {
+       "power": 108.578,
+       "wavelength": 505
+    },
+    {
+       "power": 107.802,
+       "wavelength": 510
+    },
+    {
+       "power": 106.296,
+       "wavelength": 515
+    },
+    {
+       "power": 104.79,
+       "wavelength": 520
+    },
+    {
+       "power": 106.239,
+       "wavelength": 525
+    },
+    {
+       "power": 107.689,
+       "wavelength": 530
+    },
+    {
+       "power": 106.047,
+       "wavelength": 535
+    },
+    {
+       "power": 104.405,
+       "wavelength": 540
+    },
+    {
+       "power": 104.225,
+       "wavelength": 545
+    },
+    {
+       "power": 104.046,
+       "wavelength": 550
+    },
+    {
+       "power": 102.023,
+       "wavelength": 555
+    },
+    {
+       "power": 100,
+       "wavelength": 560
+    },
+    {
+       "power": 98.1671,
+       "wavelength": 565
+    },
+    {
+       "power": 96.3342,
+       "wavelength": 570
+    },
+    {
+       "power": 96.0611,
+       "wavelength": 575
+    },
+    {
+       "power": 95.788,
+       "wavelength": 580
+    },
+    {
+       "power": 92.2368,
+       "wavelength": 585
+    },
+    {
+       "power": 88.6856,
+       "wavelength": 590
+    },
+    {
+       "power": 89.3459,
+       "wavelength": 595
+    },
+    {
+       "power": 90.0062,
+       "wavelength": 600
+    },
+    {
+       "power": 89.8026,
+       "wavelength": 605
+    },
+    {
+       "power": 89.5991,
+       "wavelength": 610
+    },
+    {
+       "power": 88.6489,
+       "wavelength": 615
+    },
+    {
+       "power": 87.6987,
+       "wavelength": 620
+    },
+    {
+       "power": 85.4936,
+       "wavelength": 625
+    },
+    {
+       "power": 83.2886,
+       "wavelength": 630
+    },
+    {
+       "power": 83.4939,
+       "wavelength": 635
+    },
+    {
+       "power": 83.6992,
+       "wavelength": 640
+    },
+    {
+       "power": 81.863,
+       "wavelength": 645
+    },
+    {
+       "power": 80.0268,
+       "wavelength": 650
+    },
+    {
+       "power": 80.1207,
+       "wavelength": 655
+    },
+    {
+       "power": 80.2146,
+       "wavelength": 660
+    },
+    {
+       "power": 81.2462,
+       "wavelength": 665
+    },
+    {
+       "power": 82.2778,
+       "wavelength": 670
+    },
+    {
+       "power": 80.281,
+       "wavelength": 675
+    },
+    {
+       "power": 78.2842,
+       "wavelength": 680
+    },
+    {
+       "power": 74.0027,
+       "wavelength": 685
+    },
+    {
+       "power": 69.7213,
+       "wavelength": 690
+    },
+    {
+       "power": 70.6652,
+       "wavelength": 695
+    },
+    {
+       "power": 71.6091,
+       "wavelength": 700
+    },
+    {
+       "power": 72.979,
+       "wavelength": 705
+    },
+    {
+       "power": 74.349,
+       "wavelength": 710
+    },
+    {
+       "power": 67.9765,
+       "wavelength": 715
+    },
+    {
+       "power": 61.604,
+       "wavelength": 720
+    },
+    {
+       "power": 65.7448,
+       "wavelength": 725
+    },
+    {
+       "power": 69.8856,
+       "wavelength": 730
+    },
+    {
+       "power": 72.4863,
+       "wavelength": 735
+    },
+    {
+       "power": 75.087,
+       "wavelength": 740
+    },
+    {
+       "power": 69.3398,
+       "wavelength": 745
+    },
+    {
+       "power": 63.5927,
+       "wavelength": 750
+    },
+    {
+       "power": 55.0054,
+       "wavelength": 755
+    },
+    {
+       "power": 46.4182,
+       "wavelength": 760
+    },
+    {
+       "power": 56.6118,
+       "wavelength": 765
+    },
+    {
+       "power": 66.8054,
+       "wavelength": 770
+    },
+    {
+       "power": 65.0941,
+       "wavelength": 775
+    },
+    {
+       "power": 63.3828,
+       "wavelength": 780
+    }
+]
